f964afa4d8b89c4155f695ce1c60dccf2fa91f27
islamshu12 <islamshu12@gmail.com>
2022-01-17 21:29:32 +0200

    f

--- app/SendMessage.php
+++ app/SendMessage.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class SendMessage extends Model
 {
-    //
+    protected $guarded=[];
+    /**
+     * Get the user that owns the SendMessage
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
