4cb0c459ddf97719d047d134cab9797286354f53
islamshu12 <islamshu12@gmail.com>
2022-01-18 23:16:28 +0200

    dd

--- resources/views/admin-dashbord/index.blade.php
+++ resources/views/admin-dashbord/index.blade.php
@@ -85,6 +85,62 @@
           </div>
         </div>
       </div>
+      @php
+      $now = Carbon\Carbon::now();
+
+    @endphp
+      <div class="col-xl-3 col-lg-6 col-12">
+        <div class="card pull-up">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="media-body text-left">
+              
+                  <h3 class="success">{{ App\Booking::whereBetween("created_at", [
+                    $now->startOfWeek()->format('Y-m-d'), //This will return date in format like this: 2022-01-10
+                    $now->endOfWeek()->format('Y-m-d')
+                 ])->count() }}</h3>
+                  <h6>{{ __('Order In this week') }}</h6>
+                </div>
+                <div>
+                  <i class="icon-user-following success font-large-2 float-right"></i>
+                </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 65%"
+                aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6 col-12">
+        <div class="card pull-up">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="media-body text-left">
+              
+                  <h3 class="info">{{ App\Booking::whereBetween("created_at", [
+                    $now->startOfMonth()->format('Y-m-d'), //This will return date in format like this: 2022-01-10
+                    $now->endOfMonth()->format('Y-m-d')
+                 ])->count() }}</h3>
+                  <h6>{{ __('Order In this Month') }}</h6>
+                </div>
+                <div>
+                  <i class="icon-user-following info font-large-2 float-right"></i>
+                </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 65%"
+                aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
    
   </div>
 @endsection
