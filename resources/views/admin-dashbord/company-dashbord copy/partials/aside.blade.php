c715179012ca1322ff948ad8fcf84137a5de4da4
islamshu12 <islamshu12@gmail.com>
2022-02-13 23:35:11 +0200

    d

--- resources/views/admin-dashbord/company-dashbord copy/partials/aside.blade.php
+++ resources/views/admin-dashbord/company-dashbord copy/partials/aside.blade.php
@@ -1,7 +1,7 @@
 <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
     <div class="main-menu-content">
       <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-        <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">العاملات</span></a>
+        <li class=" nav-item"><a href="/"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">العاملات</span></a>
           <ul class="menu-content">
             <li><a class="menu-item" href="{{ route('company.workers.index') }}" data-i18n="nav.dash.ecommerce">جميع العاملات</a>
             </li>
@@ -11,7 +11,7 @@
           </ul>
         </li>
 
-        <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">الطلبات</span></a>
+        <li class=" nav-item"><a href="/"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">الطلبات</span></a>
           <ul class="menu-content">
             <li><a class="menu-item" href="{{ route('company.orders.index') }}" data-i18n="nav.dash.ecommerce">جميع الطلبات</a>
             </li>
--- resources/views/frontend/contact_us.blade.php
+++ resources/views/frontend/contact_us.blade.php
@@ -28,7 +28,7 @@
         <div class="container">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">{{ __('Home') }}</a></li>
+                    <li class="breadcrumb-item"><a href="/">{{ __('Home') }}</a></li>
                     <li class="breadcrumb-item active" aria-current="page" >{{ __('Contact Us') }}</li>
                 </ol>
             </nav>
