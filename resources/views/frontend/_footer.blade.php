816d5b81f2a28c920a5cd53c2fc2ec04bc52cec8
islamshu12 <islamshu12@gmail.com>
2022-02-19 21:54:17 +0200

    d

--- resources/views/frontend/_footer.blade.php
+++ resources/views/frontend/_footer.blade.php
@@ -83,4 +83,16 @@
   
     </div>
   </div>
-  
+  <div class="wrapper-cover">
+    
+    <div class="wrapper">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="shadow"></div>
+        <div class="shadow"></div>
+        <div class="shadow"></div>
+        <span>...جاري التحميل</span>
+    </div>
+
+</div>
