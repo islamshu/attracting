14a5f168fca3766a63f238027628c632acad0bfd
islamshu12 <islamshu12@gmail.com>
2022-02-19 21:55:10 +0200

    d

--- public/front/js/script.js
+++ public/front/js/script.js
@@ -1,73 +1,65 @@
-$(document).ready(function () {
-    
-$(window).scroll(function(){
+$(document).ready(function() {
 
-    if($(this).scrollTop() > $('.carousel').offset().top){
-        console.log('yas')
-        $('nav').addClass('fixed-top')
-    
-    }else{
-        $('nav').removeClass('fixed-top')
+    $(window).scroll(function() {
 
-    }
+        if ($(this).scrollTop() > $('.carousel').offset().top) {
+            console.log('yas')
+            $('nav').addClass('fixed-top')
+
+        } else {
+            $('nav').removeClass('fixed-top')
+
+        }
 
 
 
-    let num=0;
-    if($(window).scrollTop() >= $('.statistic').offset().top - 200){
-    
-        
-                setInterval(function(){
-                    $('.statistic-item  h3').each(function () { 
-                        
-                    if($(this).text() < $(this).data('scroll')){
-                        num ++;
+        let num = 0;
+        if ($(window).scrollTop() >= $('.statistic').offset().top - 200) {
+
+
+            setInterval(function() {
+                $('.statistic-item  h3').each(function() {
+
+                    if ($(this).text() < $(this).data('scroll')) {
+                        num++;
                         $(this).text(num);
-                    }else{
+                    } else {
                         $(this).text($(this).data('scroll'));
                     }
 
-                    
+
                 });
 
-                },20)
-    }
+            }, 20)
+        }
 
 
 
 
 
-})
+    })
 
-$('.collapse .navbar-nav li ').click(function(){
+    $('.collapse .navbar-nav li ').click(function() {
 
-    console.log($('#'+$(this).data("link")).offset().top +1);
+        console.log($('#' + $(this).data("link")).offset().top + 1);
 
-    $('html,body').animate({
+        $('html,body').animate({
 
-        scrollTop:$('#'+$(this).data("link")).offset().top +1
+            scrollTop: $('#' + $(this).data("link")).offset().top + 1
 
-    },1000)
-
-
-})
-
- $('.collapse form').click(function(){
-     location.href='filter.html'
- })
+        }, 1000)
 
 
-new WOW().init();
+    })
+
+    $('.collapse form').click(function() {
+        location.href = 'filter.html'
+    })
+
+
+    new WOW().init();
+    $(window).on("load", function() {
+        $('.wrapper-cover').fadeOut("slow")
+    })
 
 });
-    
-
-
-
-
-
-
-
-
-
-
